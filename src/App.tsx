0ff050c230dcd0307e8635beb443719cdb5a19e7
jerrynim <tjerry3@naver.com>
2019-07-28 20:28:09 +0900

    UI && Nav &&  aws-amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import "./App.css";
-const App: React.FC = () => {
-  return (
-    <div className="App container">
-      <Navbar fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">Scratch</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-      </Navbar>
-    </div>
-  );
-};
+import Routes from "./Routes";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <Navbar collapseOnSelect>
+          <Navbar>
+            <Navbar.Brand>
+              <Link to="/">Scratch</Link>
+            </Navbar.Brand>
+          </Navbar>
+          <Navbar.Collapse>
+            <Nav>
+              <Link to="/signup">Signup</Link>
+              <Link to="/login">Login</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes />
+      </div>
+    );
+  }
+}
 
 export default App;
